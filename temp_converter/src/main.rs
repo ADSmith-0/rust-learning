db2e104214afa1b26f2d8c91370fb04116d305aa
ADSmith-0 <adam00199382818@gmail.com>
2026-05-17 00:40:48 +0100

    Added fahrenheit to celsius converter

--- temp_converter/src/main.rs
+++ temp_converter/src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-  println!("Hello, world!");
+  loop {
+    let mut input = String::new();
+
+    std::io::stdin()
+      .read_line(&mut input)
+      .expect("Failed to read line");
+
+    let input: f64 = match input.trim().parse() {
+      Ok(v) => v,
+      Err(e) => {
+        println!("Could not convert value: {e}");
+        continue;
+      }
+    };
+
+    println!("Celsius: {}", f_to_c(input));
+  }
 }
+
+fn f_to_c(f: f64) -> f64 {
+  (f - 32.0) / 1.8
+}
+
+// fn c_to_f(c: f64) -> f64 {
+//   (c * 1.8) + 32.0
+// }
