2894ccffebe66b96b22393869f9bba6c3e57cdcf
ADSmith-0 <adam00199382818@gmail.com>
2026-05-19 23:10:30 +0100

    Added celsius conversion

--- temp_converter/src/main.rs
+++ temp_converter/src/main.rs
@@ -6,7 +6,10 @@ fn main() {
       .read_line(&mut input)
       .expect("Failed to read line");
 
-    let input: f64 = match input.trim().parse() {
+    let index = input.len() - 2;
+
+    let degree = &input[..index];
+    let degree: f64 = match degree.trim().parse() {
       Ok(v) => v,
       Err(e) => {
         println!("Could not convert value: {e}");
@@ -14,7 +17,13 @@ fn main() {
       }
     };
 
-    println!("Celsius: {}", f_to_c(input));
+    let unit = &input[index..].as_bytes();
+
+    if unit[0] == b'f' {
+      println!("Celsius: {}", f_to_c(degree));
+    } else {
+      println!("Fahrenheit: {}", c_to_f(degree));
+    }
   }
 }
 
@@ -22,6 +31,6 @@ fn f_to_c(f: f64) -> f64 {
   (f - 32.0) / 1.8
 }
 
-// fn c_to_f(c: f64) -> f64 {
-//   (c * 1.8) + 32.0
-// }
+fn c_to_f(c: f64) -> f64 {
+  (c * 1.8) + 32.0
+}
